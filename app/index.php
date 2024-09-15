e79fff092517587a33ff640ba8c1c7644b40d703
Florian Dallarosa <wi23b158@technikum-wien.at>
2024-09-15 10:45:50 +0200

    updated to Bootstrap 5

--- app/index.php
+++ app/index.php
@@ -23,9 +23,7 @@ switch ($request) {
     case '/help':
         require 'src/views/help.php';
         break;
-
-<<<<<<< HEAD
-
+        
     case '/register':
         require 'src/views/auth/register.php';
         break;
@@ -33,22 +31,8 @@ switch ($request) {
     case '/login':
         require 'src/views/auth/login.php';
         break;
-    
-
-=======
-    case '/auth/register':
-        require 'src/views/auth/register.php';
-        break;
-    
-    case '/auth/login':
-        require 'src/views/auth/login.php';
-        break;
-
-    case '/test':
-        require 'src/views/test.php';
-        break;
->>>>>>> 215242aa559f7136ff15cc81073670b99ff78a4d
-
+        
+        
     default:
         http_response_code(404);
         require 'src/views/404.php';
--- app/src/views/includes/header.php
+++ app/src/views/includes/header.php
@@ -3,11 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-=======
-    <link rel="stylesheet" href="/views/styles.css">
->>>>>>> 215242aa559f7136ff15cc81073670b99ff78a4d
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="/includes/styles.css">
 </head>
 <body>
