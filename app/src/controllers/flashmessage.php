8c09b0839a96c05428dfe96470717aff6e083f7e
Florian Dallarosa <wi23b158@technikum-wien.at>
2024-09-17 18:36:57 +0200

    added close button to flashmessage

--- app/src/controllers/flashmessage.php
+++ app/src/controllers/flashmessage.php
@@ -7,7 +7,8 @@ function set_flash_message($type, $message) {
 if (isset($_SESSION['flash_messages'])) {
     foreach ($_SESSION['flash_messages'] as $type => $message) {
         $alert_class = $type === 'success' ? 'alert-success' : 'alert-danger';
-         echo "<div class='alert $alert_class' role='alert'>" . htmlspecialchars($message) . "</div>";
+         echo "<div class='alert $alert_class  alert-dismissible' role='alert'>" . htmlspecialchars($message) . " 
+         <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button> </div>";
     }
     // Flash-Nachrichten löschen
     unset($_SESSION['flash_messages']);
