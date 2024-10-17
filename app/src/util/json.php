e3fa8afbb01f45803950cef435ffe6487b03cb38
Viktor Mandlbauer <viktor.mandlbauer@protonmail.com>
2024-10-17 22:53:55 +0200

    Rooms with datepicker

--- app/src/util/json.php
+++ app/src/util/json.php
@@ -6,12 +6,13 @@ function process_json_request($args = array())
 
     if (json_last_error() !== JSON_ERROR_NONE) {
         http_response_code(400);
-        echo json_encode(['error' => 'Invalid JSON']);
+        throw new Exception('Invalid JSON');
         exit;
     }
 
     foreach ($args as $arg) {
         if (!isset($data[$arg])) {
+            http_response_code(400);
             throw new Exception('Missing ' . $arg);
         }
     }
--- db/sample.sql
+++ db/sample.sql
@@ -6,17 +6,17 @@ INSERT INTO users (pronouns, givenname, surname, username, email, role, password
 ('she/her', 'Emily', 'Davis', 'emilydavis', 'emily@example.com', 'user', 'hashedpassword5', 'salt5'),
 ('they/them', 'Taylor', 'Wilson', 'taylorwilson', 'taylor@example.com', 'user', 'hashedpassword6', 'salt6');
 
-INSERT INTO rooms (room_id, type, description, price_per_night, status) VALUES
-(201, 'Single', 'A single room with a single bed', 50.00, 'free'),
-(202, 'Double', 'A double room with a double bed', 75.00, 'booked'),
-(203, 'Suite', 'A suite with a living area and bedroom', 150.00, 'reserved'),
-(204, 'Single', 'A single room with a single bed', 50.00, 'free'),
-(205, 'Double', 'A double room with a double bed', 75.00, 'booked'),
-(206, 'Suite', 'A suite with a living area and bedroom', 150.00, 'free');
+INSERT INTO rooms (room_id, type, description, price_per_night) VALUES
+(201, 'Single', 'A single room with a single bed', 50.00),
+(202, 'Double', 'A double room with a double bed', 75.00),
+(203, 'Suite', 'A suite with a living area and bedroom', 150.00),
+(204, 'Single', 'A single room with a single bed', 50.00),
+(205, 'Double', 'A double room with a double bed', 75.00),
+(206, 'Suite', 'A suite with a living area and bedroom', 150.00);
 
 INSERT INTO bookings (user_id, room_id, check_in_date, check_out_date) VALUES
-(1, 1, '2023-10-20', '2023-10-25'),
-(2, 4, '2023-11-01', '2023-11-05'),
-(3, 5, '2023-11-10', '2023-11-15'),
-(2, 2, '2023-10-01', '2023-10-05'),
-(3, 3, '2023-10-10', '2023-10-15');
+(1, 201, '2023-10-20', '2023-10-25'),
+(2, 204, '2023-11-01', '2023-11-05'),
+(3, 205, '2023-11-10', '2023-11-15'),
+(2, 202, '2023-10-01', '2023-10-05'),
+(3, 203, '2023-10-10', '2023-10-15');
