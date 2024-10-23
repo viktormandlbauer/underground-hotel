ec89306a875cf906ea60cdf662ded26c0eb1f744
Viktor Mandlbauer <viktor.mandlbauer@gmail.com>
2024-10-23 18:18:17 +0200

    Added json util

--- app/src/util/json.php
+++ app/src/util/json.php
@@ -1,37 +1,19 @@
 <?php
 
-function json_request($params = [])
+function process_json_request($args = []): array
 {
-    global $logger;
 
-    // Check if request method is POST
-    if($_SERVER['REQUEST_METHOD'] !== 'POST') {
-        $logger->log('Invalid request method');
-        http_response_code(405);
-        echo json_encode(['error' => 'Invalid request method']);
-        exit();
-    }
-
-    // Read input data
     $data = json_decode(file_get_contents('php://input'), true);
 
-    // Check if JSON is valid
     if (json_last_error() !== JSON_ERROR_NONE) {
-        $logger->log('Invalid JSON');
         http_response_code(400);
-        echo json_encode(['error' => 'Invalid JSON']);
-        exit();
+        throw new Exception('Invalid JSON');
     }
 
-    // Check if all required parameters are set
-    if(!empty($params)) {
-        foreach($params as $param) {
-            if(!isset($data[$param])) {
-                $logger->log('Missing parameter: ' . $param);
-                http_response_code(400);
-                echo json_encode(['error' => 'Missing parameter: ' . $param]);
-                exit();
-            }
+    foreach ($args as $arg) {
+        if (!isset($data[$arg])) {
+            http_response_code(400);
+            throw new Exception('Missing ' . $arg);
         }
     }
 
