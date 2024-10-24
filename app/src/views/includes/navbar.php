c6187dffadb237e6c33291f15c01ad7b68689409
Viktor Mandlbauer <viktor.mandlbauer@gmail.com>
2024-10-24 18:50:40 +0200

    Updated navbar

--- app/src/views/includes/navbar.php
+++ app/src/views/includes/navbar.php
@@ -21,36 +21,32 @@
         <a class="nav-link" href="/help">Hilfe</a>
       </li>
     </ul>
-    <?php
-    if (!isset($_SESSION['username'])) {
-      echo '
-    <ul class="navbar-nav ms-auto">
-      <li class="nav-item">
-        <a class="btn btn-outline-primary mr-2 navbar-items-right" href="/login">Login</a>
-      </li>
-      <li class="nav-item">
-        <a class="btn btn-primary navbar-items-right" href="/register">Registrieren</a>
-      </li>
-    </ul>
-    ';
-    } else {
-      echo '
-    <ul class="navbar-nav ms-auto">
-      <li class="nav-item dropdown">
-        <a class="btn btn-outline-primary dropdown-toggle mr-2 navbar-items-right" href="#" id="user_dropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">' . htmlspecialchars($_SESSION['username']) . '</a>
-        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="user_dropdown">
-          <li><a class="dropdown-item" href="/profile">Profil anzeigen</a></li>
-          <li><a class="dropdown-item" href="/">Einstellungen</a></li>
-          <li><hr class="dropdown-divider"></li>
-          <li><a class="dropdown-item" href="#">Logout</a></li>
-        </ul>
-      </li>
-      <li class="nav-item">
-        <a class="btn btn-primary navbar-items-right" href="/logout">Logout</a>
-      </li>
-    </ul>
-    ';
-    }
-    ?>
+    <?php if (!isset($_SESSION['username'])) : ?>
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="btn btn-outline-primary mr-2 navbar-items-right" href="/login">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="btn btn-primary navbar-items-right" href="/register">Registrieren</a>
+        </li>
+      </ul>
+    <?php else: ?>
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item dropdown">
+          <a class="btn btn-outline-primary dropdown-toggle mr-2 navbar-items-right" href="#" id="user_dropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><?php echo $_SESSION['username'] ?></a>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="user_dropdown">
+            <li><a class="dropdown-item" href="/profile">Profil anzeigen</a></li>
+            <li><a class="dropdown-item" href="/">Einstellungen</a></li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#">Logout</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="btn btn-primary navbar-items-right" href="/logout">Logout</a>
+        </li>
+      </ul>
+    <?php endif ?>
   </div>
 </nav>
