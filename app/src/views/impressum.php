16b9ecc889a7a4e58678ea73b43d659624c5f0ba
Florian Dallarosa <wi23b158@technikum-wien.at>
2024-11-30 16:16:22 +0100

    help reorganized

--- app/src/views/impressum.php
+++ app/src/views/impressum.php
@@ -1,16 +1,13 @@
 <?php include 'src/views/includes/header.php'; ?>
 
-
 <body>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-9 col-md-8 col-sm-12">
-                <h1 class="text-center mt-5">Impressum</h1>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+                <h1 class="text-center mb-5">Impressum</h1>
 
-                <!--Firmen Worlaut-->
-
-                <div class="form-group">
-                    <h2>Firmen Wortlaut test</h2>
+                <div class="mb-4">
+                    <h2>Firmenwortlaut</h2>
                     <p>
                         Underground Hotel GmbH<br>
                         Musterstraße 123<br>
@@ -19,9 +16,7 @@
                     </p>
                 </div>
 
-
-                <!--Handelsregistereintrag-->
-                <div class="form-group">
+                <div class="mb-4">
                     <h2>Handelsregistereintrag</h2>
                     <p>
                         HRB 123456<br>
@@ -29,26 +24,25 @@
                         Sitz: 10115 Berlin<br>
                         Musterstraße 123 | Deutschland<br>
                         Tel.: +49 12345678<br>
-
-                        E-Mail.: <a href="email@underground-hotel.de">
-                            email@underground-hotel.de</a>
-                        <br>
+                        E-Mail: <a href="mailto:email@underground-hotel.de">email@underground-hotel.de</a><br>
                     </p>
                 </div>
 
-                <!--Vertretungbefugte-->
-                <div class="form-group">
-                    <h2>Vertretungbefugte</h2>
-                    <p>
-                        Gesellschafter Viktor Mandlbauer (51%)<br>
-                        Gesellschafter Florian Dallarosa (49%)<br>
-                    </p>
+                <div class="mb-4">
+                    <h2>Vertretungsbefugte</h2>
+                    <div class="row">
+                        <div class="col-md-6 text-center">
+                            <img src="/path" alt="Foto von Viktor Mandlbauer" class="img-fluid rounded mb-2" />
+                            <p>Gesellschafter Viktor Mandlbauer (51%)</p>
+                        </div>
+                        <div class="col-md-6 text-center">
+                            <img src="/path" alt="Foto von Florian Dallarosa" class="img-fluid rounded mb-2" />
+                            <p>Gesellschafter Florian Dallarosa (49%)</p>
+                        </div>
+                    </div>
                 </div>
-                <img src="" alt="Foto von Viktor Mandlbauer" width="50%" height="50%" />
-                <img src="" alt="Foto von Florian Dallarosa" width="50%" height="50%" />
 
-                <!--Aufsichtsbehörde-->
-                <div class="form-group">
+                <div class="mb-4">
                     <h2>Aufsichtsbehörde</h2>
                     <p>
                         Zuständige Aufsichtsbehörde:<br>
@@ -58,35 +52,32 @@
                     </p>
                 </div>
 
-                <!--Haftung Links-->
-                <div class="form-group">
+                <div class="mb-4">
                     <h3>Haftung für Links</h3>
                     <p>
-                        Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss
-                        haben.
+                        Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben.
                         Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der
                         verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich.
                     </p>
                 </div>
 
-                <!--Urherrecht-->
-                <div class="form-group">
+                <div class="mb-4">
                     <h3>Urheberrecht</h3>
                     <p>
                         Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem
-                        deutschen Urheberrecht.
-                        Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen
-                        des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
+                        deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung
+                        außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors
+                        bzw. Erstellers.
                     </p>
                 </div>
 
-                <!--Online Streitbeilegung-->
-                <div class="form-group">
+                <div class="mb-4">
                     <h2>Online-Streitbeilegung</h2>
                     <p>
                         Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:<br>
-                        <a href="https://ec.europa.eu/consumers/odr"
-                            target="_blank">https://ec.europa.eu/consumers/odr</a>.<br>
+                        <a href="https://ec.europa.eu/consumers/odr" target="_blank">
+                            https://ec.europa.eu/consumers/odr
+                        </a>.<br>
                         Unsere E-Mail-Adresse finden Sie oben im Impressum.<br>
                     </p>
                 </div>
@@ -94,4 +85,5 @@
         </div>
     </div>
 </body>
+
 <?php include 'src/views/includes/footer.php'; ?>
